e27760d48e7b48dfe6048240a3a91bdc73484351
Arctic Ice Studio <development@arcticicestudio.com>
2017-04-08 16:41:13 +0200

    GHI-#46 Add configs for multiple development dependency packages

---
+++ .stylelintrc.js
@@ -0,0 +1,26 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+title      stylelint Configuration                             +
+project    nord-atom-syntax                                    +
+repository https://github.com/arcticicestudio/nord-atom-syntax +
+author     Arctic Ice Studio                                   +
+email      development@arcticicestudio.com                     +
+copyright  Copyright (C) 2017                                  +
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+[References]
+stylelint
+  https://stylelint.io
+  https://stylelint.io/user-guide/rules
+  https://github.com/stylelint/stylelint-config-standard
+*/
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "color-hex-case": "upper",
+    "comment-empty-line-before": null,
+    "selector-type-no-unknown": [true, {
+      "ignoreTypes": ["/^atom-/"]
+    }]
+  }
+}

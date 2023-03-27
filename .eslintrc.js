0c712051ddabec4b6eab91e32191296426371434
Sven Greb <development@svengreb.de>
2023-03-27 23:59:14 +0200

    `nordtheme` organization migration (#72)
    As part of the "Northern Post - The state and roadmap of Nord" [1]
    announcement, this repository will be migrated to the `nordtheme` GitHub
    organization [2].
    This issue is a task of the nordtheme/nord#185 [3] epic (tasklist [4]).
    
    [1]: https://github.com/orgs/nordtheme/discussions/183
    [2]: https://github.com/nordtheme
    [3]: https://github.com/nordtheme/nord/issues/185
    [4]: https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/about-task-lists
    
    GH-71

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,79 +1,31 @@
 /*
- * Copyright (C) 2016-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (C) 2016-present Sven Greb <development@svengreb.de>
- *
- * Project:    Nord Atom Syntax
- * Repository: https://github.com/arcticicestudio/nord-atom-syntax
- * License:    MIT
- * References:
- *   http://eslint.org/docs/rules
- *   http://eslint.org/docs/user-guide/configuring
- *   http://eslint.org/docs/user-guide/configuring#using-configuration-files
- *   http://eslint.org/docs/user-guide/configuring#specifying-environments
- *   http://eslint.org/docs/user-guide/configuring.html#specifying-globals
+ * Copyright (c) 2016-present Sven Greb <development@svengreb.de>
+ * This source code is licensed under the MIT license found in the license file.
  */
 
+/**
+ * Configurations for ESLint.
+ * @see https://eslint.org/docs/latest/use/configure
+ * @see https://eslint.org/docs/latest/use/configure/#using-configuration-files
+ * @see https://eslint.org/docs/latest/use/configure/#specifying-environments
+ * @see https://eslint.org/docs/latest/rules
+ */
 module.exports = {
-  "env": {
-    "node": true,
-    "es6": true,
-    "browser": true,
-    "jasmine": true,
-    "atomtest": true
+  root: true,
+  env: {
+    jasmine: true,
+    atomtest: true,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 6,
-    "ecmaFeatures": {}
+  globals: {
+    atom: true,
   },
-  "plugins": [],
-  "extends": [
-    "eslint:recommended"
-  ],
-  "globals": {
-    "atom": true
-  },
-  "rules": {
-    // Style Guide
-    "array-bracket-spacing": ["error", "never"],
-    "comma-dangle": ["error", "never"],
-    "curly":["error", "all"],
-    "func-call-spacing": ["error", "never"],
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "no-duplicate-imports": "error",
-    "no-floating-decimal": "error",
-    "no-tabs": "error",
-    "no-trailing-spaces": ["error", {
-      "skipBlankLines": false
-    }],
-    "no-var": "error",
-    "no-whitespace-before-property": "error",
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-    // Error Prevention
-    "no-cond-assign": ["error", "always"],
-    "no-console": "off",
-    // Code Performance
-    "global-require": "warn",
-    // Documentation
-    "require-jsdoc": ["warn", {
-      "require": {
-        "FunctionDeclaration": true,
-        "MethodDefinition": false,
-        "ClassDeclaration": true,
-        "ArrowFunctionExpression": false
-      }
-    }],
-    "valid-jsdoc": ["error", {
-      "prefer": {
-        "arg": "param",
-        "argument": "param",
-        "class": "constructor",
-        "virtual": "abstract"
+  extends: ["@arcticicestudio/eslint-config-base", "@arcticicestudio/eslint-config-base/prettier"],
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "capitalized-comments": "off",
       },
-      "requireParamDescription": true,
-      "requireReturnDescription": true
-    }]
-  }
-}
+    },
+  ],
+};
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,24 +1,29 @@
 /*
- * Copyright (C) 2016-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (C) 2016-present Sven Greb <development@svengreb.de>
- *
- * Project:    Nord Atom Syntax
- * Repository: https://github.com/arcticicestudio/nord-atom-syntax
- * License:    MIT
- * References:
- *   https://stylelint.io
- *   https://stylelint.io/user-guide/rules
- *   https://github.com/stylelint/stylelint-config-standard
+ * Copyright (c) 2016-present Sven Greb <development@svengreb.de>
+ * This source code is licensed under the MIT license found in the license file.
  */
 
+/**
+ * Configurations for Stylelint.
+ * @see https://stylelint.io
+ * @see https://stylelint.io/user-guide/rules
+ * @see https://github.com/stylelint/stylelint-config-standard
+ */
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
-    "color-hex-case": "upper",
-    "comment-empty-line-before": null,
-    "selector-type-no-unknown": [true, {
-      "ignoreTypes": ["/^atom-/"]
-    }],
-    "selector-pseudo-element-colon-notation": "single"
-  }
-}
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-prettier"],
+  overrides: [
+    {
+      files: ["*.less"],
+      plugins: ["stylelint-less"],
+      customSyntax: "postcss-less",
+      rules: {
+        "at-rule-no-unknown": null,
+        "color-no-invalid-hex": true,
+        "less/color-no-invalid-hex": true,
+        "selector-class-pattern": null,
+        "no-descending-specificity": null,
+      },
+    },
+  ],
+};
